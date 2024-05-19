426920e43900969c7171e6cdb55c276a1d4ff6a7
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-19 17:31:14 -0500

    added refreshTokenController

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
 
+// custom middleware for logging request
 app.use(logger);
-
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
-
+// middleware for form data
 app.use(express.urlencoded({ extended: false }));
-
+// middleware for JSON
 app.use(express.json());
+// middleware for cookies
+app.use(cookieParser());
 
 // serve static files aka applies the css,image or images and text
 app.use("/", express.static(path.join(__dirname, "/public")));
@@ -22,9 +26,10 @@ app.use("/", express.static(path.join(__dirname, "/public")));
 // ROUTES
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
 app.use(verifyJWT);
 app.use("/movies", require("./routes/movieAPI/movies"));
 app.use("/users", require("./routes/getUsers"));
-
+// custom middleware for handling errors
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));

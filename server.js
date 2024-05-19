7be7bfac006dfd7b73ef854c19644961b8e227db
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-19 11:42:17 -0500

    created errorHandler middleware

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3000;
 
 app.use(logger);
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -15,4 +18,5 @@ app.use(express.json());
 app.use("/auth", require("./routes/auth"));
 app.use("/register", require("./routes/register"));
 
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));

d273933ad54e9bf3ad7edffa38de05dbcf9c2828
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-19 09:40:43 -0500

    created auth/register routes

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const PORT = process.env.PORT || 3000;
 
+app.use(logger);
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));

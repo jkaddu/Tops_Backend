ff758fc5a821a272465ad48bd75d06eac3db27a9
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-18 22:51:09 -0500

    implemented login/register

---
+++ controllers/authController.js
@@ -0,0 +1,26 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrypt = require("bcrpyt");
+
+const handleLogin = async (req, res) => {
+  const { user, password } = req.body;
+
+  if (!user || !password)
+    return res.status(400).json({ message: "Username and password required." });
+  const foundUser = usersDB.users.find((person) => person.username === user);
+  if (!foundUser) return res.sendStatus(401);
+
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    res.json({ success: `User ${user} is logged in.` });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
---
+++ controllers/registerController.js
@@ -0,0 +1,37 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("node:fs/promises");
+const path = require("path");
+const bcrpyt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+
+  const duplicate = usersDB.users.find(
+    (person) => person.username === username
+  );
+  if (duplicate) return res.sendStatus(409);
+
+  try {
+    const hashedPwd = await bcrpyt.hash(password, 10);
+    const newUser = { username: username, password: hashedPwd };
+    usersDB.setUsers([...usersDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    console.log(usersDB.users);
+    res.status(201).json({ success: `New user ${user} was created.` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};

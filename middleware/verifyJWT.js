a42147c5c72a35ea357782f729c47d59add5c29b
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-19 12:11:05 -0500

    fixed typo in verifyJWT file

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
   const authHeaders = req.headers.authorization || req.headers.Authorization;
-  if (!authheaders?.startWith("Bearer ")) return res.sendStatus(401);
+  if (!authHeaders?.startWith("Bearer ")) return res.sendStatus(401);
 
   const token = authHeaders.split(" ")[1];
   jwt.verify(token);

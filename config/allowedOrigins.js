c816db105ac2d2d9cc6974770b02f91f28f8c767
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-19 12:04:34 -0500

    put allowedOrigins in its own file

---
+++ config/allowedOrigins.js
@@ -0,0 +1,4 @@
+// For Cross Origin Resource Sharing
+const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:3000"];
+
+module.exports = allowedOrigins;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:3000"];
+const allowedOrigin = require("./allowedOrigin");
 
 const corsOptions = {
   origin: (origin, callback) => {

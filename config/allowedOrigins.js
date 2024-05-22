7dc4cbf26f2f2aeb68a5b07aab3ca9373ee4b514
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-22 08:55:28 -0500

    added domain to allowedOrigins file

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,8 @@
 // For Cross Origin Resource Sharing
-const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+  "https://glitch.com/edit/#!/tops-movie",
+];
 
 module.exports = allowedOrigins;

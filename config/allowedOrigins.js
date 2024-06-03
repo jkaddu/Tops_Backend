c2633c1d2cf2d487b7f3f161ba5dae4feec539eb
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-06-03 10:46:09 -0500

    added another origin to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://tops-backend.onrender.com",
+  "https://create-react-app-seven-drab.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
